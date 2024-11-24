e7a0ac46b41d851e86c53cd8df1fe5ea596f8eda
Hristo Taushanov <htaushanov@yahoo.com>
2024-11-24 09:38:22 +0200

    Initial commit

---
+++ test/regular.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Regular from '../lib/regular-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/regular-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Regular.RegularStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
