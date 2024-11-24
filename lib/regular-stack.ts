b7813e9e26241be59093e9514f486e27be354ea4
Hristo Taushanov <htaushanov@yahoo.com>
2024-11-24 10:11:28 +0200

    created ProcessorApi

--- lib/regular-stack.ts
+++ lib/regular-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -12,6 +13,10 @@ export class PreparationStack extends cdk.Stack {
         type: AttributeType.STRING
       },
       billingMode: BillingMode.PAY_PER_REQUEST
-    })
+    });
+
+    const api = new RestApi(this, 'ProcessorApi');
+    const resource = api.root.addResource('processJASON');
+
   }
 }
